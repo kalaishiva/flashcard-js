e387870064c2a7e7e3f1875163d2f247bc47c45c
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-04 22:41:18 +0200

    flash card project completed fully

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 let createQuestion = document.querySelector(".create_question");
 let createAnswer = document.querySelector(".create_answer");
-console.log(createAnswer);
+//console.log(createAnswer);
 
 let questionContainer = document.querySelector(".question_container");
 
 let question = document.querySelector(".question");
 let answer = document.querySelector(".answer");
+let createCardContainer = document.querySelector(".createCardContainer");
 
-/* let newItem = document.createElement('div');
-newItem.className = "itemDivss";
-console.log(newItem);
-questionContainer.append(newItem);
- */
+// let itemDiv = document.querySelectorAll(".item_div");
+
+let itemDivClone = document.getElementById("item_div_clone");
+
+itemDivClone.classList.add("hidden");
+
+
+//showCreateCard
+function showCreateCard() {
+    createCardContainer.classList.remove("hidden");
+}
+
+function hideCard() {
+    createCardContainer.classList.add("hidden");
+}
 
 //new card created using ----cloneNoded
 function newCard() {
-    let itemDiv = document.querySelector(".item_div");
-    let newDiv = itemDiv.cloneNode(true);
+    //let itemDivClone = document.getElementById("item_div_clone");
+    let newDiv = itemDivClone.cloneNode(true);
     //changing the question and answer(which is the child)
     let paraQue = newDiv.childNodes[1].childNodes[1];
     let paraAns = newDiv.childNodes[1].childNodes[3];
@@ -24,4 +35,17 @@ function newCard() {
     paraQue.innerHTML = createQuestion.value;
     paraAns.innerHTML = createAnswer.value;
     questionContainer.append(newDiv);
+    newDiv.classList.remove("hidden");
+}
+
+function deleteCard() {
+
+    let itemDiv = document.querySelectorAll(".item_div");
+
+    itemDiv.forEach((item) => {
+        item.remove();
+
+    });
+
+
 }
